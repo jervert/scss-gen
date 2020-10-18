d4e475e1100fc38bf6f5eeebcf1cf4e36fdfe86b
Antonio Rodríguez <antonio.rodriguez@outbook.es>
2020-10-18 23:36:24 +0200

    fix eslint

--- index.js
+++ index.js
@@ -5,23 +5,32 @@ const del = require('del');
 const { writeFile, mkdir } = require('fs');
 const { join } = require('path');
 const notifier = require('node-notifier');
-const { scss } = require('./config.json')
+const { scss } = require('./config.json');
 
 function sassNotifyResult(params = {}) {
   log(params.message);
   notifier.notify({
     title: `SCSS ${params.isError ? 'Error' : 'Success'}`,
     message: params.path ? `${params.event} ${params.path}` : 'First build',
-    icon: join(__dirname, `icons/icons8-${params.isError ? 'cancel': 'checked'}-512.png`)
+    icon: join(
+      __dirname,
+      `icons/icons8-${params.isError ? 'cancel': 'checked'}-512.png`
+    )
   });
 }
 
 function afterSassRender(result, params) {
   writeFile(scss.dest, result.css, error => {
     if (error) {
-      sassNotifyResult(Object.assign({}, params, { message: error, isError: true }));
+      sassNotifyResult(
+        Object.assign({}, params, { message: error, isError: true })
+      );
     } else {
-      sassNotifyResult(Object.assign({}, params, { message: `Generated: ${scss.dest}`, isError: false }));
+      sassNotifyResult(
+        Object.assign(
+          {}, params, { message: `Generated: ${scss.dest}`, isError: false }
+        )
+      );
     }
   }); 
 }
@@ -31,7 +40,9 @@ function buildCss(params = {}) {
     file: scss.src,
   }, function(error, result) {
     if (error) {
-      sassNotifyResult(Object.assign({}, params, { message: error, isError: true }));
+      sassNotifyResult(
+        Object.assign({}, params, { message: error, isError: true })
+      );
     } else {
       afterSassRender(result, params);
     }
