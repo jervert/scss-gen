0d5dec4652f57d479268bbf33a3b97161259e8f9
Antonio Rodríguez <antonio.rodriguez@outbook.es>
2020-10-19 11:49:45 +0200

    sass.notify-result test

---
+++ test/sass.notify-result.test.js
@@ -0,0 +1,60 @@
+const { join } = require('path');
+const sassNotifyResult = require('../node/sass.notify-result');
+
+const ICON_ERROR = join(
+  __dirname,
+  '../node/icons/icons8-cancel-512.png'
+);
+const ICON_SUCCESS = join(
+  __dirname,
+  '../node/icons/icons8-checked-512.png'
+);
+const TITLE_ERROR = 'SCSS error';
+const TITLE_SUCCESS = 'SCSS success';
+const MESSAGE_FIRST_BUILD = 'First build';
+
+test('sass result error, first build', () => {
+  const result = sassNotifyResult({
+    isError: true
+  });
+
+  expect(result.title).toBe(TITLE_ERROR);
+  expect(result.message).toBe(MESSAGE_FIRST_BUILD);
+  expect(result.icon).toBe(ICON_ERROR);
+});
+
+test('sass result success, first build', () => {
+  const result = sassNotifyResult({
+    isError: false
+  });
+
+  expect(result.title).toBe(TITLE_SUCCESS);
+  expect(result.message).toBe(MESSAGE_FIRST_BUILD);
+  expect(result.icon).toBe(ICON_SUCCESS);
+});
+
+test('sass result error, modify event', () => {
+  const params = {
+    isError: true,
+    path: '/main.scss',
+    event: 'modify'
+  };
+  const result = sassNotifyResult(params);
+
+  expect(result.title).toBe(TITLE_ERROR);
+  expect(result.message).toBe(`${params.event} ${params.path}`);
+  expect(result.icon).toBe(ICON_ERROR);
+});
+
+test('sass result error, modify event', () => {
+  const params = {
+    isError: false,
+    path: '/main.scss',
+    event: 'modify'
+  };
+  const result = sassNotifyResult(params);
+
+  expect(result.title).toBe(TITLE_SUCCESS);
+  expect(result.message).toBe(`${params.event} ${params.path}`);
+  expect(result.icon).toBe(ICON_SUCCESS);
+});
