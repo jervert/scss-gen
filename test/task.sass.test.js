3f0bac2d3289aafd62e829f8fbe758c953e1e5eb
Antonio Rodríguez <antonio.rodriguez@outbook.es>
2020-10-21 23:24:03 +0200

    write sass test

--- test/task.sass.test.js
+++ test/task.sass.test.js
@@ -1,9 +1,15 @@
-const { buildCss } = require('../node/task.sass').test;
+const { scss } = require('../config');
+const { buildCss, writeSass } = require('../node/task.sass').test;
+
+const CONFIG_IS_FIRST = {
+  isFirst: true
+};
+const CONFIG_IS_NOT_FIRST = {
+  isFirst: false
+};
 
 test('build css, first build', async () => {
-  const data = await buildCss({
-    isFirst: true
-  });
+  const data = await buildCss(CONFIG_IS_FIRST);
   expect(data).toEqual(expect.objectContaining({
     isFirst: true,
     result: expect.any(Object)
@@ -11,11 +17,27 @@ test('build css, first build', async () => {
 });
 
 test('build css, not first build', async () => {
-  const data = await buildCss({
-    isFirst: false
-  });
+  const data = await buildCss(CONFIG_IS_NOT_FIRST);
   expect(data).toEqual(expect.objectContaining({
     isFirst: false,
     result: expect.any(Object)
+  }));
+});
+
+test('write css, first build', async () => {
+  const data = await buildCss(CONFIG_IS_FIRST).then(writeSass);
+  expect(data).toEqual(expect.objectContaining({
+    isFirst: true,
+    result: expect.any(Object),
+    written: scss.dest
+  }));
+});
+
+test('write css, not first build', async () => {
+  const data = await buildCss(CONFIG_IS_NOT_FIRST).then(writeSass);
+  expect(data).toEqual(expect.objectContaining({
+    isFirst: false,
+    result: expect.any(Object),
+    written: scss.dest
   }));
 });
