7d2312ead3cfd086d1c80634ae285384f68a6078
Antonio Rodríguez <antonio.rodriguez@outbook.es>
2020-10-22 12:55:29 +0200

    config css min, css map

--- config.js
+++ config.js
@@ -4,6 +4,8 @@ module.exports = {
   scss: {
     src: join (__dirname, './src/scss/main.scss'),
     dest: join (__dirname, './dist/css/main.css'),
+    destMin: join (__dirname, './dist/css/main.min.css'),
+    destMap: join (__dirname, './dist/css/main.min.css.map'),
     watch: join (__dirname, './src/scss/**/*.scss')
   },
   target: {
