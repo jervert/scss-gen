13f5d534fa15b9516a421db41c7a71690f0b1a16
Antonio Rodríguez <antonio.rodriguez@outbook.es>
2020-10-22 12:56:47 +0200

    clean-css: minify and map

--- node/task.sass.js
+++ node/task.sass.js
@@ -1,5 +1,6 @@
 const sass = require('node-sass');
 const chokidar = require('chokidar');
+const CleanCSS = require('clean-css')
 const log = require('fancy-log');
 const notifier = require('node-notifier');
 const { writeFile } = require('fs');
@@ -8,7 +9,6 @@ const sassNotifyResult = require('./task.sass.notify-result');
 const { TITLE_ERROR } = require('./constants');
 
 function writeSassResolve(params) {
-  log.info(`Generated: ${scss.dest}`);
   notifier.notify(sassNotifyResult(
     Object.assign(
       {}, params, { isError: false }
@@ -19,22 +19,58 @@ function writeSassResolve(params) {
   }
 }
 
-function writeSass(params) {
+function setResult(values) {
   return new Promise(function(resolve, reject) {
+    let params = {};
+    values.forEach(value => {
+      params = Object.assign({}, params, value);
+    });
+    resolve(params);
+  });
+}
+
+function writeSass(params) {
+  const optimizedCss = new CleanCSS({
+    sourceMap: true
+  }).minify(params.result.css);
+  const writeDefault = new Promise(function(resolve, reject) {
     writeFile(scss.dest, params.result.css, error => {
       if (error) {
         reject(error, params);
       } else {
-        resolve(Object.assign({}, params, {written: scss.dest}));
+        log.info(`Generated: ${scss.dest}`);
+        resolve({ written: scss.dest });
       }
     });
-  }); 
+  });
+  const writeMin = new Promise(function(resolve, reject) {
+    writeFile(scss.destMin, optimizedCss.styles, error => {
+      if (error) {
+        reject(error, params);
+      } else {
+        log.info(`Generated: ${scss.destMin}`);
+        resolve({ writtenMin: scss.destMin });
+      }
+    });
+  });
+  const writeMap = new Promise(function(resolve, reject) {
+    writeFile(scss.destMap, optimizedCss.styles, error => {
+      if (error) {
+        reject(error, params);
+      } else {
+        log.info(`Generated: ${scss.destMap}`);
+        resolve({ writtenMap: scss.destMap });
+      }
+    });
+  });
+
+  return Promise.all([params, writeDefault, writeMin, writeMap]);
 }
 
 function buildCss(params = {}) {
   return new Promise(function(resolve, reject) {
     sass.render({
-      file: scss.src,
+      file: scss.src
     }, function(error, result) {
       if (error) {
         reject(error, params);
@@ -56,6 +92,7 @@ function sassRejected(error, params) {
 function taskSass(params = {}) {
   buildCss(params)
     .then(writeSass)
+    .then(setResult)
     .then(writeSassResolve)
     .catch(sassRejected);
 }
@@ -79,6 +116,7 @@ module.exports = {
   taskSass,
   test: {
     buildCss,
-    writeSass
+    writeSass,
+    setResult
   }
 };
