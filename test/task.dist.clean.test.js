fe808d88fa22c28fc18a40a3ea30f9a2f0331254
Antonio Rodríguez <antonio.rodriguez@outbook.es>
2020-10-24 00:46:04 +0200

    test clean

--- test/task.dist.clean.test.js
+++ test/task.dist.clean.test.js
@@ -2,7 +2,7 @@ const clean = require('../node/task.dist.clean');
 const { target } = require('../config');
 
 test('clean dist', async () => {
-  const data = await clean(target.clean);
+  const data = await clean(target.clean)();
   expect(data).toEqual(expect.objectContaining({
     clean: expect.any(Array)
   }));
