323842a4ca5e965840afdd5508e6da09be725700
Antonio Rodríguez <antonio.rodriguez@outbook.es>
2020-10-24 00:34:48 +0200

    refactor

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const { target } = require('./config');
 const clean = require('./node/task.dist.clean');
-const { taskDistCreate } = require('./node/task.dist.create');
+const distCreate = require('./node/task.dist.create');
+const { taskSass } = require('./node/task.sass');
 
-clean(target.clean).then(taskDistCreate);
+clean(target.clean)
+  .then(distCreate)
+  .then(taskSass({
+    isFirst: true
+  }));
