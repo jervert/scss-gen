0f0bf8e50730ed98e3bfcddb4a8aaa4bf58c164e
Antonio Rodríguez <antonio.rodriguez@outbook.es>
2020-10-24 00:34:29 +0200

    promise refactor

--- node/task.dist.create.js
+++ node/task.dist.create.js
@@ -1,54 +1,32 @@
 const fs = require('fs');
 const log = require('fancy-log');
-const { taskSass } = require('./task.sass');
 const { MESSAGE_CREATED_DIR, MESSAGE_DIR_EXISTS } = require('./constants');
 const { target } = require('../config');
 
-function distCreate() {
+module.exports = function(params) {
   return new Promise(function(resolve, reject) {
     fs.access(target.css, fs.constants.F_OK, error => {
       if (error) {
         log.info(`Folder to be created: ${target.css}`);
         fs.mkdir(target.css, { recursive: true }, error => {
           if (error) {
-            reject({
-              message: `Folder cannot be created: ${target.css}. ${error}`
-            });
+            log.error(`Folder cannot be created: ${target.css}. ${error}`);
+            reject(error);
           } else {
-            resolve({
-              message: `${MESSAGE_CREATED_DIR}: ${target.css}`
-            });
+            const message = `${MESSAGE_CREATED_DIR}: ${target.css}`
+            log.info(message);
+            resolve(Object.assign({}, params, {
+              create: message
+            }));
           }
         });
       } else {
-        resolve({
-          message: `${MESSAGE_DIR_EXISTS}: ${target.css}`
-        });
+        const message = `${MESSAGE_DIR_EXISTS}: ${target.css}`;
+        log.info(message);
+        resolve(Object.assign({}, params, {
+          create: message
+        }));
       }
     });
   });
-}
-
-function createResolved({ message }) {
-  log.info(message);
-  taskSass({
-    isFirst: true
-  });
-}
-
-function createRejected({ message }) {
-  log.error(message);
-}
-
-function taskDistCreate() {
-  distCreate()
-    .then(createResolved)
-    .catch(createRejected);
-}
-
-module.exports = {
-  taskDistCreate,
-  test: {
-    distCreate
-  }    
 };
