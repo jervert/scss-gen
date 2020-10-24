2d2baf08017c8eae2f95ea133f538504823607a0
Antonio Rodríguez <antonio.rodriguez@outbook.es>
2020-10-24 17:21:34 +0200

    watch message

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const { target } = require('./config');
 const clean = require('./node/task.dist.clean');
 const distCreate = require('./node/task.dist.create');
 const { taskSass } = require('./node/task.sass');
+const log = require('fancy-log');
 
 Promise.resolve()
   .then(clean(target.clean))
   .then(distCreate)
   .then(taskSass({
     isFirst: true
-  }));
+  }))
+  .then(() => log.info('Watching changes'));
