c9912af4c7bb595685272d34c28509969b04745d
Antonio Rodríguez <antonio.rodriguez@outbook.es>
2020-10-25 00:20:45 +0200

    figlet+chalk

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const { target } = require('./config');
+const figlet = require('figlet');
+const chalk = require('chalk');
 const clean = require('./node/task.dist.clean');
 const distCreate = require('./node/task.dist.create');
 const { taskSass } = require('./node/task.sass');
@@ -10,4 +12,14 @@ Promise.resolve()
   .then(taskSass({
     isFirst: true
   }))
-  .then(() => log.info('Watching changes'));
+  .then(() => {
+    log.info('Watching changes');
+    figlet.text('SCSS-Gen', function(error, data) {
+      if (error) {
+        console.log('Something went wrong...');
+        console.dir(error);
+        return;
+      }
+      console.log(chalk.cyan(data));
+    });
+  });
